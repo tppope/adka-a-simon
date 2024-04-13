6f41fb3eb8f4752f09cf4cd798b14ae911f767fb
tppope <popik.tomas@icloud.com>
2024-04-14 00:50:55 +0200

    Project prepared to start making frontend

---
+++ database/migrations/2024_04_13_144049_create_guests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->boolean('is_lactose_free');
+            $table->boolean('is_gluten_free');
+            $table->boolean('is_vegetarian');
+            $table->boolean('is_vegan');
+            $table->boolean('is_child');
+            $table->tinyText('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guests');
+    }
+};
