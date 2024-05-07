e50d0daecc004d2718322dcaf12cefa4876513c2
KatarinaPopikova <katka.popikova@gmail.com>
2024-05-07 23:55:31 +0200

    feat: add favicon

--- resources/app.blade.php
+++ resources/app.blade.php
@@ -4,6 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('storage/favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('storage/favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('storage/favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('storage/favicon/site.webmanifest') }}">
+
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
